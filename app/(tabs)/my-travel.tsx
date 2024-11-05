f0e2f5054d8bdaa8df91d83fdece2c3e7b73a034
YouSH42 <enddl402@gmail.com>
2024-11-05 13:35:13 +0900

    마이스케줄 UI 작업 브랜치 생성

--- app/(tabs)/my-travel.tsx
+++ app/(tabs)/my-travel.tsx
@@ -6,7 +6,6 @@ import { View, Text } from 'react-native';
 export default function MyTravelScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <HeaderBar isMain={false} title="내 여행"/>
       <Text>내 여행</Text>
     </View>
   );
